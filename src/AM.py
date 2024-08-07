bd7403efb44f03143ea69ad22e7dcd14dc133992
Ibrahimjr74 <166428087+Ibrahimjr74@users.noreply.github.com>
2024-08-07 20:46:11 +0300

    first commit

---
+++ src/AM.py
@@ -0,0 +1,14 @@
+gtst = "Day7"
+print('Hello Today is our',gtst,'course')
+list = [0 ,2, 4]
+
+print('The First Even number is:',list[0])
+print('The Second Even number is:',list[1])
+print('The Third Even number is:',list[2])
+
+fruits = {"apple": 10, "banana": 15, "pineapple": 20}
+choice = input("Enter your fruit choice: ")
+value = fruits.get(choice, "Fruit not found")
+print(f"The value of {choice} is: {value} birr")
+
+
